be492a39af6960246d4be8266a1aba7fe51506b4
Abhishek kumawat <abhikumardambiwal@gmail.com>
2025-01-30 07:19:02 +0530

    cron

--- utilities/cron.js
+++ utilities/cron.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const Cron_keep_server_alive = async (req, res) => {
   try {
-    await axios.get(`${process.env.Server}`);
+    await axios.get(`${process.env.Server}/`);
   } catch (error) {
     console.log(`Cron error ${error}`);
   }
